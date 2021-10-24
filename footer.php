ba82eecde6301451cab3317ef49b831e73e07a8f
GF Development <gfdeveloper14@gmail.com>
2021-10-24 18:11:08 +0700

    commit

---
+++ footer.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Footer file.
+ * 
+ * @package Aquila  
+ */
+
+?>
+
+<footer>Footer</footer>
+
+<?php wp_footer(); ?>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -5,20 +5,14 @@
  * 
  * @package Aquila
  */
+
+get_header();
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
+<div class="content">
+    Content
+</div>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>WordPress Aquila</title>
-</head>
-
-<body>
-    Aquila
-</body>
-
-</html>
+<?php
+get_footer();
+?>
