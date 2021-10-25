4b22b6d1b9866aa0488a3e168d60b76dd0075915
GF Development <gfdeveloper14@gmail.com>
2021-10-25 09:32:35 +0700

    commit

--- function.php
+++ function.php
@@ -5,3 +5,14 @@
  * 
  * @package Aquila
  */
+
+echo 'prev';
+print_r(get_template_directory_uri());
+wp_die();
+
+function aquila_enqueue_scripts()
+{
+    wp_enqueue_style('stylesheet', get_template_directory_uri() . 'style.css');
+}
+
+add_action('wp_enqueue_scripts', 'aquila_enqueue_scripts');
